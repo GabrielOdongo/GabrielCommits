5b89d12f7fe3c930fbe306f91db4afae5aa39652
GabrielOdongo <gabrielodongo152@gmail.com>
2020-07-11 15:31:01 +0300

    ya leo

---
+++ messigoals.js
@@ -0,0 +1,16 @@
+//Use variables to find the sum of the goals Messi scored in 3 competitions
+
+//Information
+//Messi goal scoring statistics:
+
+//Competition	Goals
+//La Liga	43
+//Champions League	10
+//Copa del Rey 5
+
+//https://www.codewars.com/kata/55ca77fa094a2af31f00002a
+
+function goal(sum) {
+    return ([43 +10, 10 + 5, 43 + 5, 43 + 10 + 5]);
+}
+console.log(goal([43 +10, 10 + 5, 43 + 5, 43 + 10 + 5]));
